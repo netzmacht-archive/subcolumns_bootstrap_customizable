0ff9c92ce19616a047d4dac1bcfaeecf68395862
David Molineus <mail@netzmacht.de>
2014-05-05 13:32:57 +0200

    fix typo in config name

--- src/system/modules/subcolumns_bootstrap_customizable/runonce/runonce.php
+++ src/system/modules/subcolumns_bootstrap_customizable/runonce/runonce.php
@@ -7,7 +7,7 @@ class upgrade
 	public function run()
 	{
 		// fix typo
-		if($GLOBALS['CONFIG']['subcolumns'] == 'bootstrap_customizable') {
+		if($GLOBALS['CONFIG']['subcolumns'] == 'boostrap_customizable') {
 			$config = \Config::getInstance();
 			$config->update('$GLOBALS[\'TL_CONFIG\'][\'subcolumns\']', 'bootstrap_customizable');
 			$config->save();
